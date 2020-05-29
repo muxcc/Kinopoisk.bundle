00874999b7ea9d39e91b0e5200871ceafc73db19
Gitea <muxcc@aumm.ru>
2020-05-30 00:25:36 +0300

    fix: add FanArtSource

--- Contents/Libraries/Shared/kinoplex/sources/__init__.py
+++ Contents/Libraries/Shared/kinoplex/sources/__init__.py
@@ -3,9 +3,9 @@ from base import SourceBase
 from kinopoisk import KinopoiskSource
 from kptrailers import KPTrailersSource
 from tmdb import TMDBSource
+from fanart import FanArtSource
 
 #from tvdb import TVDBSource
 #from freebase import FreebaseSource
 #from itunes import ITunesSource
-#from fanart import FanArtSource
 #from mm import MovieManiaSource
